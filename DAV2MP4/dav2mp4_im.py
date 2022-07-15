ae5c2a38296e5113bb63f6d8d5e6e62510980dc2
Rakesh Ranjan <109340487+ranjan430@users.noreply.github.com>
2022-07-15 22:27:23 +0530

    Add files via upload

---
+++ DAV2MP4/dav2mp4_im.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+"""
+    Purpose             : To convert DAV to MP4.
+    Author              : Rakesh Ranjan
+    Last Edited         : 15 July 2022
+    Edited By           : Rakesh Ranjan
+"""
+
+print ("Converting all of the .dav files in this current directory into .mp4 files using ffmpeg")	
+import os
+from subprocess import call
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    ext = f.split(".")[-1]
+    if ext == "dav" or ext == "DAV":
+    	mp4Name = f.replace("dav", "mp4")
+    	print ("Converting: " + f)
+    	call(['ffmpeg', '-y', '-i', f, "-vcodec", "libx264", "-crf", "24", mp4Name])
+    	print ("Converted,Enjoy in mp4 now: " + f)
